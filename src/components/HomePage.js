7555190aecea17803f32f425cb3783f1d9a7aa92
unknown <rahul.kannoujia@mobilecoderz.com>
2023-07-31 14:17:13 +0530

    first commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,15 @@
 // src/components/HomePage.js
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-
 const HomePage = () => {
   const [popularMovies, setPopularMovies] = useState([]);
-
+  const fetchAPi = async () => {
+    return await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=c45a857c193f6302f2b5061c3b85e743&language=en-US&page=1"
+    ).then((res) => res.json());
+  };
   useEffect(() => {
-    // Fetch popular movies from API
-    // Use the URL provided in the API Details section
-    // Remember to append your API key to the URL
-    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=c45a857c193f6302f2b5061c3b85e743&language=en-US&page=1')
-      .then(response => {
-        setPopularMovies(response.data.results);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+   fetchAPi().then(res=>setPopularMovies(res?.results))
   }, []);
 
   return (
@@ -24,7 +17,7 @@ const HomePage = () => {
       <h2>Home Page / Popular Movie Page</h2>
       {/* Display list of popular movies */}
       <ul>
-        {popularMovies.map(movie => (
+        {popularMovies.length>0 && popularMovies.map(movie => (
           <li key={movie.id}>
             <Link to={`/movie/${movie.id}`}>
               <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
